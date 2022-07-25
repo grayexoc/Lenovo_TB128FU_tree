35641e77936a45a0d3016e2b037cbfb75498d79a
grayexoc <64415928+grayexoc@users.noreply.github.com>
2022-07-25 17:33:15 +0900

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -8,5 +8,9 @@
 LOCAL_PATH := $(call my-dir)
 
 ifeq ($(TARGET_DEVICE),TB128FU)
-include $(call all-subdir-makefiles,$(LOCAL_PATH))
+
+include $(call all-makefiles-under,$(LOCAL_PATH))
+
+include $(CLEAR_VARS)
+
 endif
