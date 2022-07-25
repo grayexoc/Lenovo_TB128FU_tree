29484993244378cf4af8d80e510c0761631dcbf8
grayexoc <64415928+grayexoc@users.noreply.github.com>
2022-07-25 13:25:33 +0900

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-add_lunch_combo omni_TB128FU-user
-add_lunch_combo omni_TB128FU-userdebug
-add_lunch_combo omni_TB128FU-eng
+add_lunch_combo twrp_TB128FU-user
+add_lunch_combo twrp_TB128FU-userdebug
+add_lunch_combo twrp_TB128FU-eng
